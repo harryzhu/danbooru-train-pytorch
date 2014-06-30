2df24d4fb8fa818ff3f9622c3e24f2207fc21bf3
unknown <v-hazhu@v-hazhu.fareast.corp.microsoft.com>
2014-07-01 00:54:07 +0800

    reset

---
+++ app/config/config.php
@@ -0,0 +1,21 @@
+<?php
+define('APP_ROOT','D:\Dev\www\php\console');
+
+return new \Phalcon\Config(array(
+    'database' => array(
+        'adapter'     => 'Mysql',
+        'host'        => 'localhost',
+        'username'    => 'root',
+        'password'    => '',
+        'dbname'      => 'test',
+    ),
+    'application' => array(
+        'controllersDir' => __DIR__ . '/../../app/controllers/',
+        'modelsDir'      => __DIR__ . '/../../app/models/',
+        'viewsDir'       => __DIR__ . '/../../app/views/',
+        'pluginsDir'     => __DIR__ . '/../../app/plugins/',
+        'libraryDir'     => __DIR__ . '/../../app/library/',
+        'cacheDir'       => __DIR__ . '/../../app/cache/',
+        'baseUri'        => '/console/',
+    )
+));
---
+++ app/views/index.volt
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Console">
+        <meta name="author" content="Harry">
+        <title></title>
+
+        <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    </head>
+
+	<body>
+{{ partial("partials/nav") }}
+
+		{{ content() }}
+	
+        <script src="assets/bootstrap/js/jquery-2.1.1.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.js"></script>
+
+</body>
+</html>
