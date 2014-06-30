3189e8f58be55b32e40387c6cdaa817954e1397d
unknown <v-hazhu@v-hazhu.fareast.corp.microsoft.com>
2014-06-30 23:56:01 +0800

    reset

---
+++ app/cache/d__dev_www_php_console_app_views_index.volt.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Console">
+        <meta name="author" content="Harry">
+        <title></title>
+
+        <link href="public/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    </head>
+
+	<body>
+<?php echo $this->partial('partials/nav'); ?>
+
+		<?php echo $this->getContent(); ?>
+	
+        <script src="public/assets/bootstrap/js/jquery-2.1.1.min.js"></script>
+        <script src="public/assets/bootstrap/js/bootstrap.js"></script>
+
+</body>
+</html>
---
+++ app/controllers/ServerController.php
@@ -0,0 +1,12 @@
+<?php
+
+class ServerController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
